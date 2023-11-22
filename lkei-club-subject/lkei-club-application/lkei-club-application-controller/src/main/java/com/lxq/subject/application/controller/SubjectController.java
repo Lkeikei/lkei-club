ee22712c636c91742ac0df209a1b8f397b0e0d75
娄须强 <loudasha0626@163.com>
2023-11-23 00:22:05 +0800

    课程添加

--- lkei-club-subject/lkei-club-application/lkei-club-application-controller/src/main/java/com/lxq/subject/application/controller/SubjectController.java
+++ lkei-club-subject/lkei-club-application/lkei-club-application-controller/src/main/java/com/lxq/subject/application/controller/SubjectController.java
@@ -1,7 +1,10 @@
 package com.lxq.subject.application.controller;
 
+import com.lxq.subject.infra.basic.service.SubjectCategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author: 娄须强
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SubjectController {
 
+    @Resource
+    private SubjectCategoryService service;
+
     @GetMapping("/test")
     public String test() {
-        return "hello";
+        return service.queryById(1L).getCategoryName();
     }
 }
--- lkei-club-subject/lkei-club-starter/src/main/java/com/lxq/subject/SubjectApplication.java
+++ lkei-club-subject/lkei-club-starter/src/main/java/com/lxq/subject/SubjectApplication.java
@@ -1,5 +1,6 @@
 package com.lxq.subject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.lxq")
+@MapperScan("com.lxq.**.mapper")
 public class SubjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubjectApplication.class);
